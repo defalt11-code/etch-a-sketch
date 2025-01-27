44bbbc34c992a64d9daa008ed7f5d7660ce254dd
defalt11-code <royilao1114@gmail.com>
2025-01-27 14:20:30 +0800

    add the button to create grid and the past reference in javascript file

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,14 @@
 /* Etch a sketch odin project */
+const createGridBtn = document.querySelector(".create-grid");
 
+createGridBtn.addEventListener("click", () =>) {
+  
+}
+
+const container = document.querySelector(".container");
+
+function addGrid() {
+
+}
 
  
