e38af43e6dec0b635281843d35c54da1172d1fd3
defalt11-code <royilao1114@gmail.com>
2025-01-27 14:40:29 +0800

    create a for loop that iterate gridcound * grindcout e.g (16x16) that create a div inside that loop

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,36 @@
 /* Etch a sketch odin project */
 const createGridBtn = document.querySelector(".create-grid");
 
-createGridBtn.addEventListener("click", () =>) {
-  
-}
+createGridBtn.addEventListener("click", () => {
+  const gridCount = parseInt(prompt("Enter a grid up to 1 - 100"));
+  container.innerHTML = "" ;
+  addGrid(gridCount);
+});
 
 const container = document.querySelector(".container");
 
-function addGrid() {
+function addGrid(gridCount) {
+  if(gridCount < 1 || gridCount > 100) {
+    return; // quit the addgrid function if gridcount is either greater or less than to 1 and 100
+  };
 
-}
+  const gridsSize = 694 / gridCount;
+  console.log(gridsSize)
+
+  for(let i = 0; i < gridCount * gridCount; i++) {
+    const grids = document.createElement("div");
+
+    grids.style.border = "1px solid lightgrey";
+    grids.style.width = `${gridsSize}px`;
+    grids.style.height = `${gridsSize}px`;
+
+    grids.addEventListener("mouseover", () => {
+      grids.style.backgroundColor = "lightgrey";
+    })
+
+    container.appendChild(grids)
+  }
+
+};
 
  
