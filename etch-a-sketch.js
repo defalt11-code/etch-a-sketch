1f7d02afa03df0bbf169767417deab12d1680e12
defalt11-code <royilao1114@gmail.com>
2025-01-27 16:17:38 +0800

    fix the math randon * 300
    change the 300 to 256

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -63,9 +63,9 @@ function randomColor() {
   for(let grid of grids) {
     grid.addEventListener("mouseover", () => {
       grid.style.backgroundColor = `
-        rgb(${Math.floor(Math.random() * 300)},
-        ${Math.floor(Math.random() * 300)},
-        ${Math.floor(Math.random() * 300)})`
+        rgb(${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)})`
     })
   }
 };
